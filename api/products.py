844f7c74a5ffb185bb3b9eefb4662f0ab168b21d
Vladimir <omkomp@mail.ru>
2025-04-24 23:47:27 +0600

    тключено Unicode-экранирование в api/products.py

--- api/products.py
+++ api/products.py
@@ -8,4 +8,4 @@ def get_products():
         {"item_id": "1", "name": "Картина 1", "price": 100},
         {"item_id": "2", "name": "Картина 2", "price": 150}
     ]
-    return jsonify(products)
+    return jsonify(products), 200, {'Content-Type': 'application/json; charset=utf-8'}
