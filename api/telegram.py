379e0affb997e89d5a6164c2f0c492852668f2c7
Vladimir <omkomp@mail.ru>
2025-04-24 15:56:20 +0600

    овторный деплой api/telegram.py

--- api/telegram.py
+++ api/telegram.py
@@ -3,34 +3,29 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackQueryHandler, CallbackContext
 
 app = Flask(__name__)
-updater = Updater("7981458266:AAGp5jIgvf_KHN_P_7pBURBnYqrT-X89mNQ", use_context=True)  # Убрали "bot"
+updater = Updater("7981458266:AAGp5jIgvf_KHN_P_7pBURBnYqrT-X89mNQ", use_context=True)
 dp = updater.dispatcher
 
-# Обработчик команды /start
 def start(update: Update, context: CallbackContext) -> None:
     keyboard = [[InlineKeyboardButton("Купить картину", callback_data='buy_painting')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Добро пожаловать в магазин!', reply_markup=reply_markup)
 
-# Обработчик нажатия на кнопку
 def button_callback(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     query.answer()
     if query.data == 'buy_painting':
         query.message.reply_text('Выберите картину!')
 
-# Регистрация обработчиков
 dp.add_handler(CommandHandler('start', start))
 dp.add_handler(CallbackQueryHandler(button_callback))
 
-# Маршрут для Telegram webhook
 @app.route('/telegram', methods=['POST'])
 def webhook():
     update = Update.de_json(request.get_json(force=True), updater.bot)
     dp.process_update(update)
     return 'OK'
 
-# Корневой маршрут для проверки
 @app.route('/')
 def index():
     return 'Bot is running'
